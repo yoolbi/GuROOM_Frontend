05ab9327d1f678e493509d27713aeb8a508fd38d
hethegrace0310 <hethegrace0310@gmail.com>
2022-10-02 15:37:11 -0400

    first page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StartPage from "./pages/StartPage";
+import InitialSetup_1 from "./pages/InitialSetup_1";
+import InitialSetup_2 from "./pages/InitialSetup_2";
+import InitialSetup_3 from "./pages/InitialSetup_3";
+import InitialSetup_snapshot from "./pages/InitialSetup_snapshot";
+import InitialSetup_done from "./pages/InitialSetup_done";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/InitialSetup_1" element={<InitialSetup_1 />} />
+        <Route path="/InitialSetup_2" element={<InitialSetup_2 />} />
+        <Route path="/InitialSetup_3" element={<InitialSetup_3 />} />
+        <Route
+          path="/InitialSetup_snapshot"
+          element={<InitialSetup_snapshot />}
+        />
+        <Route path="/InitialSetup_done" element={<InitialSetup_done />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/InitialSetup_2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const initialSetup_2 = () => {
+  return <div></div>;
+};
+
+export default initialSetup_2;
