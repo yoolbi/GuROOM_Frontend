8d46bdb1d03d6a7c3a3c6edd19c6a52fcb8f0d9e
hethegrace0310 <hethegrace0310@gmail.com>
2022-10-03 01:03:28 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import StartPage from "./pages/StartPage";
-import InitialSetup_1 from "./pages/InitialSetup_1";
-import InitialSetup_2 from "./pages/InitialSetup_2";
-import InitialSetup_3 from "./pages/InitialSetup_3";
-import InitialSetup_snapshot from "./pages/InitialSetup_snapshot";
-import InitialSetup_done from "./pages/InitialSetup_done";
+import InitialSetup from "./pages/InitialSetup";
+import Homepage from "./pages/Homepage";
 import "./index.css";
 
 function App() {
@@ -13,14 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<StartPage />} />
-        <Route path="/InitialSetup_1" element={<InitialSetup_1 />} />
-        <Route path="/InitialSetup_2" element={<InitialSetup_2 />} />
-        <Route path="/InitialSetup_3" element={<InitialSetup_3 />} />
-        <Route
-          path="/InitialSetup_snapshot"
-          element={<InitialSetup_snapshot />}
-        />
-        <Route path="/InitialSetup_done" element={<InitialSetup_done />} />
+        <Route path="/InitialSetup" element={<InitialSetup />} />
+        <Route path="/Homepage" element={<Homepage />} />
       </Routes>
     </Router>
   );
--- src/pages/InitialSetup_2.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const initialSetup_2 = () => {
-  return <div></div>;
-};
-
-export default initialSetup_2;
