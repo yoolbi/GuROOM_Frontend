83dfc4bdd28e29769a86d690a17f87bcb8f33990
Yool Bi Lee <yoolbi716@gmail.com>
2022-10-05 01:59:24 -0400

    select group

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -11,26 +11,32 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
 // import { Link } from "react-router-dom";
 
 const Homepage = () => {
   const [value, setValue] = React.useState("home");
-
   const handleChangeTab = (event, newValue) => {
     setValue(newValue);
   };
 
   const [drive, setDrive] = React.useState("google");
-
   const handleChange = (event) => {
     setDrive(event.target.value);
   };
 
   const [openGroup, setOpenGroup] = React.useState(true);
-
   const handleClickOpenGroup = () => {
     setOpenGroup(!openGroup);
     console.log(openGroup);
+  };
+
+  const [group, setGroup] = React.useState("");
+  const handleSelectGroup = (event) => {
+    setGroup(event.target.value);
   };
 
   return (
@@ -50,7 +56,7 @@ const Homepage = () => {
             style={{
               borderRight: "1px solid #DBDBDB",
               width: "30%",
-              minWidth: "250px",
+              // minWidth: "420px",
             }}
             // style={{ border: "1px solid black", width: "30%", minWidth: "250px" }}
           >
@@ -83,6 +89,47 @@ const Homepage = () => {
                 }}
                 onClick={handleClickOpenGroup}
               ></img>
+            </div>
+            <div className="groupSelect">
+              <div style={{ marginLeft: "60%", color: "#8A8D92" }}>
+                2022.12.23 12:00:05
+              </div>
+              <Box
+                sx={{ width: "96.5%" }}
+                style={{ margin: "5px 0px 15px 13px" }}
+              >
+                <FormControl sx={{ width: "96.5%" }} size="small">
+                  <InputLabel id="baseFileSelect">Group Snapshot</InputLabel>
+                  <Select
+                    labelId="group-select"
+                    id="group-select"
+                    value={group}
+                    label="group snapshot"
+                    onChange={handleSelectGroup}
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+              <Paper
+                component="form"
+                sx={{
+                  width: "93%",
+                  height: 40,
+                }}
+                style={{ marginLeft: "13px" }}
+              >
+                <IconButton type="button" aria-label="search">
+                  <SearchIcon />
+                </IconButton>
+                <InputBase
+                  sx={{ width: "80%" }}
+                  placeholder="Group name, domain, user"
+                  inputProps={{ "aria-label": "search google maps" }}
+                />
+              </Paper>
             </div>
           </div>
         ) : (
@@ -152,7 +199,11 @@ const Homepage = () => {
                 <Tab label="Access Control" value="accessControl" />
                 <Tab label="Compare Snapshots" value="compareSnapshots" />
                 {value === "home" ? (
-                  <div style={{ width: "55%" }}></div>
+                  openGroup ? (
+                    <div style={{ width: "55%" }}></div>
+                  ) : (
+                    <div style={{ width: "68%" }}></div>
+                  )
                 ) : (
                   <div style={{ width: "70%" }}></div>
                 )}
