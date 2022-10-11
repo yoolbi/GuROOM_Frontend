0f6b138c07e2f47915e78ca539b9da66c4a8aca7
hethegrace0310 <hethegrace0310@gmail.com>
2022-10-11 17:36:28 -0400

    compare snapshot

--- src/pages/CompareSnapshots.js
+++ src/pages/CompareSnapshots.js
@@ -11,6 +11,35 @@ import Divider from "@mui/material/Divider";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import { Button, InputAdornment } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    fail: {
+      main: "#ef9a9a",
+      contrastText: "#000",
+    },
+    success: {
+      main: "#81c784",
+      contrastText: "#000",
+    },
+    basic: {
+      main: "#eeeeee",
+      contrastText: "#000",
+    },
+    failText: {
+      main: "#ff6e40",
+    },
+    successText: {
+      main: "#81c784",
+    },
+  },
+});
 
 const CompareSnapshots = () => {
   const [baseFileFirst, setBaseFileFisrt] = useState("");
@@ -74,64 +103,368 @@ const CompareSnapshots = () => {
             </Select>
           </FormControl>
         </Box>
-      </div>
-      <div>
-        <Box
-          sx={{ width: "25%", bgcolor: "background.paper", paddingTop: "15px" }}
+        <Button
+          variant="contained"
+          size="small"
+          style={{ height: "35px", marginTop: "10px", marginLeft: "10px" }}
         >
-          <Divider />
-          <List component="nav" aria-label="secondary mailbox folder">
-            <ListItemButton
-              selected={selectedIndex === 1}
-              onClick={(event) => handleListItemClick(event, 1)}
-            >
-              <div role="presentation" onClick={handleClickOpenBreadcrumb}>
-                <Breadcrumbs maxItems={2} aria-label="breadcrumb">
-                  <Link underline="hover" color="inherit" href="#">
-                    Home
-                  </Link>
-                  <Link underline="hover" color="inherit" href="#">
-                    Catalog
-                  </Link>
-                  <Link underline="hover" color="inherit" href="#">
-                    Accessories
-                  </Link>
-                  <Link underline="hover" color="inherit" href="#">
-                    New Collection
-                  </Link>
-                  <Typography color="text.primary">Belts</Typography>
-                </Breadcrumbs>
-              </div>
-            </ListItemButton>
+          COMPARE
+        </Button>
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <Box
+            sx={{
+              width: "270px",
+              bgcolor: "background.paper",
+              paddingTop: "15px",
+            }}
+          >
             <Divider />
-            <ListItemButton
-              selected={selectedIndex === 2}
-              onClick={(event) => handleListItemClick(event, 2)}
-            >
-              <ListItemText primary="My Drive / ... / Lecture1.pdf" />
-            </ListItemButton>
-            <Divider />
-            <ListItemButton
-              selected={selectedIndex === 3}
-              onClick={(event) => handleListItemClick(event, 3)}
-            >
-              <ListItemText primary="My Drive / ... / Lecture1.pdf" />
-            </ListItemButton>
-            <Divider />
-            <ListItemButton
-              selected={selectedIndex === 4}
-              onClick={(event) => handleListItemClick(event, 4)}
-            >
-              <ListItemText primary="My Drive / ... / Lecture1.pdf" />
-            </ListItemButton>
-            <ListItemButton
-              selected={selectedIndex === 5}
-              onClick={(event) => handleListItemClick(event, 5)}
-            >
-              <ListItemText primary="My Drive / ... / Lecture1.pdf" />
-            </ListItemButton>
-          </List>
-        </Box>
+            <List component="nav" aria-label="secondary mailbox folder">
+              <ListItemButton
+                selected={selectedIndex === 1}
+                onClick={(event) => handleListItemClick(event, 1)}
+              >
+                <div role="presentation" onClick={handleClickOpenBreadcrumb}>
+                  <Breadcrumbs maxItems={2} aria-label="breadcrumb">
+                    <Link underline="hover" color="inherit" href="#">
+                      Home
+                    </Link>
+                    <Link underline="hover" color="inherit" href="#">
+                      Catalog
+                    </Link>
+                    <Link underline="hover" color="inherit" href="#">
+                      Accessories
+                    </Link>
+                    <Link underline="hover" color="inherit" href="#">
+                      New Collection
+                    </Link>
+                    <Typography color="text.primary">Belts</Typography>
+                  </Breadcrumbs>
+                </div>
+              </ListItemButton>
+              <Divider />
+              <ListItemButton
+                selected={selectedIndex === 2}
+                onClick={(event) => handleListItemClick(event, 2)}
+              >
+                <ListItemText primary="My Drive / ... / Lecture1.pdf" />
+              </ListItemButton>
+              <Divider />
+              <ListItemButton
+                selected={selectedIndex === 3}
+                onClick={(event) => handleListItemClick(event, 3)}
+              >
+                <ListItemText primary="My Drive / ... / Lecture1.pdf" />
+              </ListItemButton>
+              <Divider />
+              <ListItemButton
+                selected={selectedIndex === 4}
+                onClick={(event) => handleListItemClick(event, 4)}
+              >
+                <ListItemText primary="My Drive / ... / Lecture1.pdf" />
+              </ListItemButton>
+              <ListItemButton
+                selected={selectedIndex === 5}
+                onClick={(event) => handleListItemClick(event, 5)}
+              >
+                <ListItemText primary="My Drive / ... / Lecture1.pdf" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </div>
+        <div
+          style={{
+            display: "grid",
+            width: "45%",
+            marginLeft: "20px",
+            marginTop: "20px",
+          }}
+        >
+          <TextField
+            id="outlined-multiline-flexible"
+            label="File Name"
+            multiline
+            maxRows={4}
+            value="{fileName}"
+            InputProps={{
+              readOnly: true,
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Owner"
+            multiline
+            maxRows={4}
+            value=""
+            InputProps={{
+              readOnly: true,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Stack direction="row" spacing={1}>
+                    <Chip avatar={<Avatar>M</Avatar>} label="Avatar" />
+                  </Stack>
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Inherit Permissions"
+            multiline
+            maxRows={4}
+            value=""
+            InputProps={{
+              readOnly: true,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Stack direction="row" spacing={1}>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="basic"
+                      />
+                    </ThemeProvider>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="fail"
+                      />
+                    </ThemeProvider>
+                  </Stack>
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Direct Permissions"
+            multiline
+            maxRows={4}
+            value=""
+            InputProps={{
+              readOnly: true,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Stack direction="row" spacing={1}>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="fail"
+                      />
+                    </ThemeProvider>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="basic"
+                      />
+                    </ThemeProvider>
+                  </Stack>
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Sharing Differences"
+            multiline
+            maxRows={4}
+            value=""
+            InputProps={{
+              readOnly: true,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Stack direction="row" spacing={1}>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="fail"
+                      />
+                    </ThemeProvider>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="fail"
+                      />
+                    </ThemeProvider>
+                  </Stack>
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <ThemeProvider theme={theme}>
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Deviant Permissions"
+              multiline
+              maxRows={4}
+              value="50%"
+              InputProps={{
+                readOnly: true,
+              }}
+              sx={{
+                "& .MuiInputBase-root": {
+                  color: "failText.main",
+                },
+              }}
+              style={{ marginBottom: "15px", width: "95%" }}
+            />
+          </ThemeProvider>
+        </div>
+        <div style={{ display: "grid", width: "45%", marginTop: "20px" }}>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="File Name"
+            multiline
+            maxRows={4}
+            value="{fileName}"
+            InputProps={{
+              readOnly: true,
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Owner"
+            multiline
+            maxRows={4}
+            value=""
+            InputProps={{
+              readOnly: true,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Stack direction="row" spacing={1}>
+                    <Chip avatar={<Avatar>M</Avatar>} label="Avatar" />
+                  </Stack>
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Inherit Permissions"
+            multiline
+            maxRows={4}
+            value=""
+            InputProps={{
+              readOnly: true,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Stack direction="row" spacing={1}>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="basic"
+                      />
+                    </ThemeProvider>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="success"
+                      />
+                    </ThemeProvider>
+                  </Stack>
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Direct Permissions"
+            multiline
+            maxRows={4}
+            value=""
+            InputProps={{
+              readOnly: true,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Stack direction="row" spacing={1}>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="success"
+                      />
+                    </ThemeProvider>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="basic"
+                      />
+                    </ThemeProvider>
+                  </Stack>
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Sharing Differences"
+            multiline
+            maxRows={4}
+            value=""
+            InputProps={{
+              readOnly: true,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Stack direction="row" spacing={1}>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="success"
+                      />
+                    </ThemeProvider>
+                    <ThemeProvider theme={theme}>
+                      <Chip
+                        avatar={<Avatar>M</Avatar>}
+                        label="Avatar"
+                        color="success"
+                      />
+                    </ThemeProvider>
+                  </Stack>
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginBottom: "15px", width: "95%" }}
+          />
+          <ThemeProvider theme={theme}>
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Deviant Permissions"
+              multiline
+              maxRows={4}
+              value="50%"
+              InputProps={{
+                readOnly: true,
+              }}
+              sx={{
+                "& .MuiInputBase-root": {
+                  color: "successText.main",
+                },
+              }}
+              style={{ marginBottom: "15px", width: "95%" }}
+            />
+          </ThemeProvider>
+        </div>
       </div>
     </div>
   );
