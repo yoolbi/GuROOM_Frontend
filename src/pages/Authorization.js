8af356321c1dd4a8859bb6232f5d7e08d6d01371
hethegrace0310 <hethegrace0310@gmail.com>
2022-10-11 01:39:32 -0400

    compare snapshot

--- src/pages/Authorization.js
+++ src/pages/Authorization.js
@@ -1,8 +1,18 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const Authorization = () => {
-  const { state, code } = useParams();
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const code = searchParams.get("code");
+    const state = searchParams.get("state");
+    console.log("code: ", code);
+    console.log("state: ", state);
+
+    // BE에 받은 code(access_code) 보내주기
+    // post해서 받은 걸 확인하고 괜찮으면 redirect
+  }, []);
 
   useEffect(() => {
     console.log("state: ", state);
