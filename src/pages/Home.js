cc48c0f5e1b9d0194e2455b0793fa1ec9ea18eb7
hethegrace0310 <hethegrace0310@gmail.com>
2022-10-10 22:57:23 -0400

    Home table

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,9 @@ import Select from "@mui/material/Select";
 // import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import FilterModal from "./FilterModal";
-
+import { DataGrid } from "@mui/x-data-grid";
+import { Button, CircularProgress } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 const style = {
   position: "absolute",
   top: "50%",
@@ -29,12 +31,103 @@ const style = {
 const Home = () => {
   const [fileSnapshot, setFileSnapshot] = React.useState("");
   const [open, setOpen] = React.useState(false);
+  const [openTakingSnapshot, setOpenTakingSnapshot] = React.useState(false);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   const handleChange = (event) => {
     setFileSnapshot(event.target.value);
+  };
+
+  //Table
+  const [pageSize, setPageSize] = React.useState(10);
+  const columns = [
+    { field: "name", headerName: "Name", width: 200 },
+    {
+      field: "owner",
+      headerName: "Owner",
+      width: 130,
+      sortable: false,
+    },
+    {
+      field: "inheritPermissions",
+      headerName: "Inherit Permission",
+      width: 150,
+      sortable: false,
+    },
+    {
+      field: "directPermission",
+      headerName: "Direct Permission",
+      width: 150,
+      sortable: false,
+    },
+    {
+      field: "sharingDifferences",
+      headerName: "Sharing Differnece",
+      description:
+        "The differences between the file’s permissions and the folder’s permissions.",
+      sortable: false,
+      width: 150,
+      // valueGetter: (params) =>
+      //   `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+    {
+      field: "deviantPermissions",
+      headerName: "Deviant Permissions",
+      description:
+        "The differences between this file’s permissions and the permissions of most other files in the folder",
+      width: 200,
+    },
+    {
+      field: "created",
+      headerName: "Created",
+      width: 120,
+    },
+    {
+      field: "modified",
+      headerName: "Modified",
+      width: 120,
+    },
+    {
+      field: "size",
+      headerName: "Size",
+      width: 80,
+    },
+    {
+      field: "edit",
+      headerName: "",
+      width: 50,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
+  const takingSnapshot = () => {
+    setOpenTakingSnapshot(true);
+    console.log("open");
+  };
+
+  const takingSnapshotClose = () => {
+    setOpenTakingSnapshot(false);
+    console.log("close");
   };
 
   return (
@@ -104,9 +197,99 @@ const Home = () => {
         </Box>
         <img
           src="/img/snapshot_image.png"
-          style={{ width: "30px", height: "30px", paddingTop: "5px" }}
+          style={{
+            width: "30px",
+            height: "30px",
+            paddingTop: "5px",
+            cursor: "pointer",
+          }}
+          onClick={takingSnapshot}
         />
       </div>
+      <div
+        style={{
+          marginTop: "10px",
+          marginLeft: "5px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>My Drive/ 어쩌고 저쩌고/ CSE 416</div>
+        <Button variant="contained" size="small">
+          EDIT
+        </Button>
+      </div>
+      <div style={{ height: "100%", marginTop: "10px" }}>
+        <div style={{ height: 630, width: "100%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={pageSize}
+            onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+            rowsPerPageOptions={[10, 20, 30]}
+            pagination
+            checkboxSelection
+          />
+        </div>
+      </div>
+      <Modal
+        open={openTakingSnapshot}
+        onClose={takingSnapshotClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div
+          style={{
+            width: "280px",
+            height: "95px",
+            backgroundColor: "white",
+            borderRadius: "5px",
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            marginRight: "20px",
+          }}
+        >
+          <div
+            style={{
+              width: "290px",
+              height: "45px",
+              backgroundColor: "#323232",
+              color: "white",
+              lineHeight: "45px",
+              fontFamily: "Roboto",
+              borderRadius: "5px 5px 0px 0px",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <div style={{ paddingLeft: "20px" }}>Taking Snapshot</div>
+            <ClearIcon
+              style={{ marginTop: "10px", width: "50px", cursor: "pointer" }}
+              onClick={takingSnapshotClose}
+            ></ClearIcon>
+          </div>
+
+          <div
+            style={{
+              width: "290px",
+              height: "50px",
+              backgroundColor: "white",
+              lineHeight: "50px",
+
+              fontFamily: "Roboto",
+              borderRadius: "0px 0px 5px 5px",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <div style={{ paddingLeft: "20px" }}>Snapshot #1</div>
+            <div style={{ marginRight: "20px", marginTop: "5px" }}>
+              <CircularProgress size={20} />
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
