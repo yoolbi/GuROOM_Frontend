caa7d6bb7e0b7708a4409034e78a465c9d14a2ec
Yool Bi Lee <yoolbi716@gmail.com>
2022-10-11 16:37:17 -0400

    create local environment

--- src/api/client.js
+++ src/api/client.js
@@ -1,3 +1,4 @@
+import urlJoin from "url-join";
 function parseJSON(response) {
   return response
     .json()
@@ -5,34 +6,48 @@ function parseJSON(response) {
 }
 
 export const getAuthorizeAPIMethod = () => {
-  return fetch("https://guroom.live/apps/auth/v1/google/authorize", {
-    credentials: "include",
-  }).then(parseJSON);
+  return fetch(
+    urlJoin(
+      process.env.REACT_APP_BACKEND_URL + "/apps/auth/v1/google/authorize"
+    ),
+    {
+      credentials: "include",
+    }
+  ).then(parseJSON);
 };
 
 export const getUserAPIMethod = () => {
-  return fetch("https://guroom.live/apps/auth/v1/google/user", {
-    credentials: "include",
-  }).then(parseJSON);
+  return fetch(
+    urlJoin(process.env.REACT_APP_BACKEND_URL, "/apps/auth/v1/google/user"),
+    {
+      credentials: "include",
+    }
+  ).then(parseJSON);
 };
 
 export const postAccessTokenAPIMethod = (code) => {
-  return fetch("https://guroom.live/apps/auth/v1/google/login", {
-    credentials: "include",
-    method: "POST",
-    body: JSON.stringify({ code: code }),
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-  }).then(parseJSON);
+  return fetch(
+    urlJoin(process.env.REACT_APP_BACKEND_URL + "/apps/auth/v1/google/login"),
+    {
+      credentials: "include",
+      method: "POST",
+      body: JSON.stringify({ code: code }),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    }
+  ).then(parseJSON);
 };
 
 export const deleteRevokeAPIMethod = () => {
-  return fetch("https://guroom.live/apps/auth/v1/google/revoke", {
-    credentials: "include",
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-  }).then(parseJSON);
+  return fetch(
+    urlJoin(process.env.REACT_APP_BACKEND_URL, "/apps/auth/v1/google/revoke"),
+    {
+      credentials: "include",
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    }
+  ).then(parseJSON);
 };
