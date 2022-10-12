142afe54e37ef8f8bf5973df98f6f9956f918062
hethegrace0310 <hethegrace0310@gmail.com>
2022-10-11 22:47:23 -0400

    fix file permission edit modal

--- src/pages/FilePermissionEditModal.js
+++ src/pages/FilePermissionEditModal.js
@@ -5,7 +5,6 @@ import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import SingleFilePermissionEdit from "./SingleFilePermissionEdit";
-import MultipleFilePermissionEdit from "./MultipleFilePermissionEdit";
 
 const FilePermissionEditModal = () => {
   const [value, setValue] = React.useState("1");
@@ -20,15 +19,11 @@ const FilePermissionEditModal = () => {
         <TabContext value={value}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab label="Single File Permission Edit" value="1" />
-              <Tab label="Multiple Files Permission Edit" value="2" />
+              <Tab label="File Permission Edit" value="1" />
             </TabList>
           </Box>
           <TabPanel value="1">
             <SingleFilePermissionEdit />
-          </TabPanel>
-          <TabPanel value="2">
-            <MultipleFilePermissionEdit />
           </TabPanel>
         </TabContext>
       </Box>
