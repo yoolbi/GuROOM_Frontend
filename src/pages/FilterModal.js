93406ac5b915055cc66534f6bf8e5015eed3f801
Yool Bi Lee <yoolbi716@gmail.com>
2022-10-24 00:32:45 -0400

    making query generator

--- src/pages/FilterModal.js
+++ src/pages/FilterModal.js
@@ -6,7 +6,6 @@ import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import QueryBuilder from "./QueryBuilder";
 import AccessControlPolicy from "./AccessControlPolicy";
-import { Button } from "@mui/material";
 
 // eslint-disable-next-line react/prop-types
 const FilterModal = ({ handleCloseSearchFilter, setSearchInput }) => {
@@ -26,39 +25,19 @@ const FilterModal = ({ handleCloseSearchFilter, setSearchInput }) => {
               <Tab label="ACCESS CONTROL POLICY" value="2" />
             </TabList>
           </Box>
-          <TabPanel value="1" style={{ paddingTop: "10px" }}>
-            <QueryBuilder setSearchInput={setSearchInput} />
+          <TabPanel
+            value="1"
+            style={{ paddingTop: "10px", paddingBottom: "0px" }}
+          >
+            <QueryBuilder
+              setSearchInput={setSearchInput}
+              handleCloseSearchFilter={handleCloseSearchFilter}
+            />
           </TabPanel>
           <TabPanel value="2">
             <AccessControlPolicy />
           </TabPanel>
         </TabContext>
-
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          {value === "1" ? (
-            <Button variant="contained">GENERATE</Button>
-          ) : (
-            <Button variant="contained">APPLY</Button>
-          )}
-
-          <div style={{ width: "10px" }}></div>
-          <Button
-            variant="contained"
-            sx={{
-              backgroundColor: "#E0E0E0",
-              color: "black",
-              "&:hover": { backgroundColor: "#E0E0E0" },
-            }}
-            onClick={handleCloseSearchFilter}
-          >
-            CANCEL
-          </Button>
-        </div>
       </Box>
     </>
   );
--- src/pages/QueryBuilder.js
+++ src/pages/QueryBuilder.js
@@ -11,13 +11,20 @@ import {
   Box,
   Autocomplete,
   Switch,
+  Button,
 } from "@mui/material";
 
 const sharingTypes = ["None", "Anything", "Individual", "Domain"];
 
-const QueryBuilder = () => {
+// eslint-disable-next-line react/prop-types
+const QueryBuilder = ({ handleCloseSearchFilter, setSearchInput }) => {
+  let query = "";
+  const [sharedDrive, setSharedDrive] = React.useState("");
   const [owner, setOwner] = React.useState("");
   const [creator, setCreator] = React.useState("");
+  const [sharedFrom, setSharedFrom] = React.useState("");
+  const [sharedTo, setSharedTo] = React.useState("");
+  const [name, setName] = React.useState("");
   // const [readable, setReadable] = React.useState("");
   // const [writeable, setWriteable] = React.useState("");
   // const [sharable, setSharable] = React.useState("");
@@ -155,21 +162,57 @@ const QueryBuilder = () => {
     { title: "Monty Python and the Holy Grail", year: 1975 },
   ];
 
+  const handleChangeSharedDrive = (event) => {
+    setSharedDrive(event.target.value);
+  };
+
   const handleChangeOwner = (event) => {
     setOwner(event.target.value);
   };
+
   const handleChangeCreator = (event) => {
     setCreator(event.target.value);
   };
-  // const handleChangeReadable = (event) => {
-  //   setReadable(event.target.value);
-  // };
-  // const handleChangeWriteable = (event) => {
-  //   setWriteable(event.target.value);
-  // };
-  // const handleChangeSharable = (event) => {
-  //   setSharable(event.target.value);
-  // };
+
+  const handleChangeSharedFrom = (event) => {
+    setSharedFrom(event.target.value);
+  };
+
+  const handleChangeSharedTo = (event) => {
+    setSharedTo(event.target.value);
+  };
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  let readable = "";
+  const handleChangeReadable = (event) => {
+    console.log(event.target.innerText);
+    if (readable === "") {
+      readable = event.target.innerText;
+    } else {
+      readable = readable + "," + event.target.innerText;
+    }
+  };
+
+  let writable = "";
+  const handleChangeWritable = (event) => {
+    if (writable === "") {
+      writable = event.target.innerText;
+    } else {
+      writable = writable + "," + event.target.innerText;
+    }
+  };
+
+  let sharable = "";
+  const handleChangeSharable = (event) => {
+    if (sharable === "") {
+      sharable = event.target.innerText;
+    } else {
+      sharable = sharable + "," + event.target.innerText;
+    }
+  };
 
   const handleChangePath = (event) => {
     setPath(event.target.value);
@@ -194,6 +237,11 @@ const QueryBuilder = () => {
     setCheckedMyDrive(!event.target.checked);
   };
 
+  const [checkedGroup, setCheckedGroup] = React.useState(false);
+  const handleChangeCheckedGroup = (event) => {
+    setCheckedGroup(event.target.checked);
+  };
+
   const handleChangeIndividual = (event) => {
     setIndividual(event.target.value);
   };
@@ -202,212 +250,283 @@ const QueryBuilder = () => {
     setDomain(event.target.value);
   };
 
+  const handleClickGenerate = () => {
+    checkedMyDrive && (query = query + "drive:MyDrive and ");
+    checkedSharedDrive && (query = query + "drive:" + sharedDrive + " and ");
+    owner && (query = query + "owner:" + owner + " and ");
+    creator && (query = query + "creator:" + creator + " and ");
+    sharedFrom && (query = query + "sharedFrom:" + sharedFrom + " and ");
+    sharedTo && (query = query + "sharedTo:" + sharedTo + " and ");
+    readable && (query = query + "readable:" + readable + " and ");
+    writable && (query = query + "writable:" + writable + " and ");
+    sharable && (query = query + "sharable:" + sharable + " and ");
+    name && (query = query + "name:" + name + " and ");
+    pathSelect === "inFolder" && (query = query + "inFolder:" + path + " and ");
+    pathSelect === "folder" && (query = query + "folder:" + path + " and ");
+    sharingTypeSelect === "None" && (query = query + "sharing:none and ");
+    sharingTypeSelect === "Anyone" && (query = query + "sharing:anyone and ");
+    sharingTypeSelect === "Individual" &&
+      (query = query + "sharing:individual:" + individual + " and ");
+    sharingTypeSelect === "Domain" &&
+      (query = query + "sharing:domain:" + domain + " and ");
+
+    query = query.slice(0, -5);
+    console.log(query);
+    query = setSearchInput(query);
+    handleCloseSearchFilter();
+  };
+
   return (
-    <div style={{ height: 570, overflowY: "scroll" }}>
-      <div>
-        <FormControlLabel control={<Switch />} label="Group" />
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
+    <div style={{ height: "100%" }}>
+      <div style={{ height: 575, overflowY: "scroll" }}>
+        <div>
           <FormControlLabel
             control={
-              <Checkbox
-                checked={checkedMyDrive}
-                onChange={handleChangeCheckboxMyDrive}
+              <Switch
+                checked={checkedGroup}
+                onChange={handleChangeCheckedGroup}
               />
             }
-            label="My Drive"
+            label="Group"
           />
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={checkedMyDrive}
+                  onChange={handleChangeCheckboxMyDrive}
+                />
+              }
+              label="My Drive"
+            />
 
-          <FormControlLabel
-            style={{ marginRight: "5px" }}
-            control={
-              <Checkbox
-                checked={checkedSharedDrive}
-                onChange={handleChangeCheckboxSharedDrive}
+            <FormControlLabel
+              style={{ marginRight: "5px" }}
+              control={
+                <Checkbox
+                  checked={checkedSharedDrive}
+                  onChange={handleChangeCheckboxSharedDrive}
+                />
+              }
+              label="Shared Drive"
+            />
+            {checkedSharedDrive && (
+              <TextField
+                label="Shared Drive Name"
+                id="outlined-start-adornment"
+                sx={{ width: "300px", marginBottom: "15px" }}
+                value={sharedDrive}
+                onChange={handleChangeSharedDrive}
               />
-            }
-            label="Shared Drive"
-          />
-          {checkedSharedDrive && (
+            )}
+          </div>
+        </div>
+        <div>
+          <div>
             <TextField
-              label="Shared Drive Name"
+              id="outlined-textareas"
+              label="Owner"
+              placeholder="username"
+              multiline
+              value={owner}
+              onChange={handleChangeOwner}
+              sx={{ width: "545px", marginBottom: "15px" }}
+            />
+          </div>
+          <div>
+            <TextField
+              id="outlined-textareas"
+              label="Creator"
+              placeholder="username"
+              multiline
+              value={creator}
+              onChange={handleChangeCreator}
+              sx={{ width: "545px", marginBottom: "15px" }}
+            />
+          </div>
+          <div>
+            <TextField
+              label="shared"
               id="outlined-start-adornment"
-              sx={{ width: "300px", marginBottom: "15px" }}
+              sx={{ width: "545px", marginBottom: "15px" }}
+              value={sharedFrom}
+              onChange={handleChangeSharedFrom}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">From</InputAdornment>
+                ),
+              }}
             />
-          )}
-        </div>
-      </div>
-      <div>
-        <div>
-          <TextField
-            id="outlined-textareas"
-            label="Owner"
-            placeholder="username"
-            multiline
-            value={owner}
-            onChange={handleChangeOwner}
-            sx={{ width: "545px", marginBottom: "15px" }}
-          />
-        </div>
-        <div>
-          <TextField
-            id="outlined-textareas"
-            label="Creator"
-            placeholder="username"
-            multiline
-            value={creator}
-            onChange={handleChangeCreator}
-            sx={{ width: "545px", marginBottom: "15px" }}
-          />
-        </div>
-        <div>
-          <TextField
-            label="shared"
-            id="outlined-start-adornment"
-            sx={{ width: "545px", marginBottom: "15px" }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">From</InputAdornment>
-              ),
-            }}
-          />
-        </div>
+          </div>
 
+          <div>
+            <TextField
+              label="shared"
+              id="outlined-start-adornment"
+              sx={{ width: "545px", marginBottom: "15px" }}
+              value={sharedTo}
+              onChange={handleChangeSharedTo}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">To</InputAdornment>
+                ),
+              }}
+            />
+          </div>
+        </div>
         <div>
-          <TextField
-            label="shared"
-            id="outlined-start-adornment"
-            sx={{ width: "545px", marginBottom: "15px" }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">To</InputAdornment>
-              ),
-            }}
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={top100Films}
+            getOptionLabel={(option) => option.title}
+            filterSelectedOptions
+            onChange={handleChangeReadable}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Readable by"
+                placeholder="Email/Name"
+                sx={{ width: "545px", marginBottom: "20px" }}
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={top100Films}
+            getOptionLabel={(option) => option.title}
+            filterSelectedOptions
+            onChange={handleChangeWritable}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Writable by"
+                placeholder="Email/Name"
+                sx={{ width: "545px", marginBottom: "20px" }}
+              />
+            )}
+          />
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={top100Films}
+            getOptionLabel={(option) => option.title}
+            filterSelectedOptions
+            onChange={handleChangeSharable}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Sharable by"
+                placeholder="Email/Name"
+                sx={{ width: "545px", marginBottom: "20px" }}
+              />
+            )}
           />
         </div>
-      </div>
-      <div>
-        <Autocomplete
-          multiple
-          id="tags-outlined"
-          options={top100Films}
-          getOptionLabel={(option) => option.title}
-          filterSelectedOptions
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Readable by"
-              placeholder="Email/Name"
-              sx={{ width: "545px", marginBottom: "20px" }}
-            />
-          )}
-        />
-        <Autocomplete
-          multiple
-          id="tags-outlined"
-          options={top100Films}
-          getOptionLabel={(option) => option.title}
-          filterSelectedOptions
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Writable by"
-              placeholder="Email/Name"
-              sx={{ width: "545px", marginBottom: "20px" }}
-            />
-          )}
-        />
-        <Autocomplete
-          multiple
-          id="tags-outlined"
-          options={top100Films}
-          getOptionLabel={(option) => option.title}
-          filterSelectedOptions
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="Sharable by"
-              placeholder="Email/Name"
-              sx={{ width: "545px", marginBottom: "20px" }}
-            />
-          )}
-        />
-      </div>
-      <Box>
-        <TextField
-          id="outlined-basic"
-          label="(file) Name"
-          variant="outlined"
-          style={{ width: "545px", marginBottom: "15px" }}
-        />
-      </Box>
-      <div>
-        <FormControl sx={{ marginBottom: "15px" }}>
-          <InputLabel id="demo-multiple-name-label">Path</InputLabel>
-          <Select
-            labelId="demo-multiple-name-label"
-            id="demo-multiple-name"
-            value={pathSelect}
-            onChange={handleChangePathType}
-            label="Path"
-            sx={{ width: "180px" }}
-          >
-            <MenuItem key="inFolder" value="inFolder">
-              inFolder
-            </MenuItem>
-            <MenuItem key="folder" value="folder">
-              folder
-            </MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          id="outlined-textareas"
-          label="Path"
-          placeholder="path"
-          multiline
-          value={path}
-          onChange={handleChangePath}
-          style={{ width: "365px" }}
-        />
-
-        <FormControl sx={{ marginBottom: "15px" }}>
-          <InputLabel id="demo-multiple-name-label">Sharing</InputLabel>
-          <Select
-            labelId="demo-multiple-name-label"
-            id="demo-multiple-name"
-            value={sharingTypeSelect}
-            onChange={handleChangeSharingType}
-            label="Sharing"
-            sx={{ width: "180px" }}
-          >
-            {sharingTypes.map((name) => (
-              <MenuItem key={name} value={name}>
-                {name}
+        <Box>
+          <TextField
+            id="outlined-basic"
+            label="(file) Name"
+            variant="outlined"
+            style={{ width: "545px", marginBottom: "15px" }}
+            value={name}
+            onChange={handleChangeName}
+          />
+        </Box>
+        <div>
+          <FormControl sx={{ marginBottom: "15px" }}>
+            <InputLabel id="demo-multiple-name-label">Folder Type</InputLabel>
+            <Select
+              labelId="demo-multiple-name-label"
+              id="demo-multiple-name"
+              value={pathSelect}
+              onChange={handleChangePathType}
+              label="Folder"
+              sx={{ width: "180px" }}
+            >
+              <MenuItem key="inFolder" value="inFolder">
+                inFolder
               </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        {sharingTypeSelect === "Individual" && (
+              <MenuItem key="folder" value="folder">
+                folder
+              </MenuItem>
+            </Select>
+          </FormControl>
           <TextField
             id="outlined-textareas"
-            label="Individual Name"
-            placeholder="username"
+            label="Path"
+            placeholder="path"
             multiline
-            value={individual}
-            onChange={handleChangeIndividual}
+            value={path}
+            onChange={handleChangePath}
             style={{ width: "365px" }}
           />
-        )}
-        {sharingTypeSelect === "Domain" && (
-          <TextField
-            id="outlined-textareas"
-            label="Domain Address"
-            placeholder="username"
-            multiline
-            value={domain}
-            onChange={handleChangeDomain}
-            style={{ width: "365px" }}
-          />
-        )}
+
+          <FormControl sx={{ marginBottom: "15px" }}>
+            <InputLabel id="demo-multiple-name-label">Sharing</InputLabel>
+            <Select
+              labelId="demo-multiple-name-label"
+              id="demo-multiple-name"
+              value={sharingTypeSelect}
+              onChange={handleChangeSharingType}
+              label="Sharing"
+              sx={{ width: "180px" }}
+            >
+              {sharingTypes.map((name) => (
+                <MenuItem key={name} value={name}>
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {sharingTypeSelect === "Individual" && (
+            <TextField
+              id="outlined-textareas"
+              label="Individual Name"
+              placeholder="username"
+              multiline
+              value={individual}
+              onChange={handleChangeIndividual}
+              style={{ width: "365px" }}
+            />
+          )}
+          {sharingTypeSelect === "Domain" && (
+            <TextField
+              id="outlined-textareas"
+              label="Domain Address"
+              placeholder="username"
+              multiline
+              value={domain}
+              onChange={handleChangeDomain}
+              style={{ width: "365px" }}
+            />
+          )}
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "30px",
+        }}
+      >
+        <Button variant="contained" onClick={handleClickGenerate}>
+          GENERATE
+        </Button>
+
+        <div style={{ width: "10px" }}></div>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: "#E0E0E0",
+            color: "black",
+            "&:hover": { backgroundColor: "#E0E0E0" },
+          }}
+          onClick={handleCloseSearchFilter}
+        >
+          CANCEL
+        </Button>
       </div>
     </div>
   );
